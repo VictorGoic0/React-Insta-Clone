757054ba5ec94d4c7ed0149cbd82f36c47aaa222
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-18 18:16:32 -0500

    file structure for components added

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from './components/SearchBar/SearchBar';
+import PostContainer from './components/PostContainer/PostContainer'
+import dummyData from './dummy-data';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <SearchBar />
+        <PostContainer />
       </div>
     );
   }
---
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CommentSection.css';
+
+const CommentSection = props => {
+  return (
+    <p>Comment Section</p>
+  )
+}
+
+export default CommentSection;
