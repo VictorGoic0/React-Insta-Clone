71b43f5591a19b03e864a8d4e47979fc3cda0154
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-02-20 17:28:37 -0500

    successfully implemented addNewComment and handleChanges methods to CommentSection component.

--- instagram/src/components/CommentSection/CommentSection.js
+++ instagram/src/components/CommentSection/CommentSection.js
@@ -7,15 +7,44 @@ class CommentSection extends React.Component {
   constructor(props) {
     super();
     this.props = props;
+    this.state = {
+      comments: this.props.comments,
+      timestamp: this.props.timestamp,
+      text: ''
+    }
+  }
+
+  handleChanges = e => {
+    console.log(this.state)
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  addNewComment = (e, i) => {
+    e.preventDefault();
+
+    const newComment = {
+      username: "commenting_specialist",
+      text: this.state.text
+    }
+    this.setState({
+      comments: [...this.state.comments, newComment],
+      timestamp: this.state.timestamp
+    })
   }
 
   render() {
     return (
       <div>
-        {this.props.comments.map((input, index) => <Comment key={index} info={input} />)}
-        <p>{this.props.timestamp}</p>
-        <form>
+        {this.state.comments.map((input, index) => <Comment key={index} info={input} />)}
+        <p>{this.state.timestamp}</p>
+        <form onSubmit={this.addNewComment}>
           <input
+          name="text"
+          type="text"
+          value={this.state.text}
+          onChange={this.handleChanges}
           placeholder="Add a comment...">
           </input>
         </form>
